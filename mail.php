39898cee13d299c4759204b91e3aefabec586cf4
leuel-tes <tesfayeleuel@gmail.com>
2022-12-08 19:23:23 +0300

    this all commits are about editing some minor things

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "tesfayeleuel@gmail.com";
+$sitename = "Leuel Tesfaye";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
