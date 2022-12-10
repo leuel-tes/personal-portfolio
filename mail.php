64b31a98757a8cef0a368a170e2404ede37a8062
leuel-tes <tesfayeleuel@gmail.com>
2022-12-10 13:11:07 +0300

    last edits
    on this commit I have edited one page html file

--- mail.php
+++ mail.php
@@ -10,3 +10,4 @@ $message = "Name: $name \nEmail: $email \nText: $text";
 
 $pagetitle = "New message from the \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
